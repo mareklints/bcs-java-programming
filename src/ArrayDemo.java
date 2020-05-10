9611b78c7daee33fc0f0ad7e89e9c18ee504f77b
Marek Lints <marek.lints@gmail.com>
2020-05-10 13:12:34 +0300

    Massiivid

---
+++ src/ArrayDemo.java
@@ -0,0 +1,12 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        String[][] seasons = {
+                {"Kevad", "Roheline"},
+                {"Suvi", "Roheline"},
+                {"Sügis", "Pruun"},
+                {"Talv", "Valge"}
+        };
+        seasons[1][1] = "Kollane";
+        System.out.println(seasons[1][1]);
+    }
+}
