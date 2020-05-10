d1f4125c2cd52b45ab934f9ae97b90054141a6ce
Marek Lints <marek.lints@gmail.com>
2020-05-10 10:34:58 +0300

    Tekstitöötlus 1

---
+++ src/TextProcessing.java
@@ -0,0 +1,8 @@
+public class TextProcessing {
+    public static void main(String[] args) {
+        String myText = "Isa ütles: \"Tule siia!\"";
+        System.out.println(myText);
+        String locationOfMySecretFile = "C:\\secret\\file.txt";
+        System.out.println(locationOfMySecretFile);
+    }
+}
