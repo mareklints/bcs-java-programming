1c0233fe7f8dc5efda01fcea2018b28146e89ece
Marek Lints <marek.lints@gmail.com>
2020-05-17 15:06:47 +0300

    Kollektsioonid

---
+++ src/CollectionsDemo.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class CollectionsDemo {
+    public static void main(String[] args) {
+
+        // List
+        List<String> names = new ArrayList<>();
+        names.add("Kati");
+        names.add("Mati");
+        names.add("Kristo");
+        System.out.println(names);
+        names.remove("Kristo");
+        System.out.println(names);
+        names.set(1, "Mari");
+        System.out.println(names);
+
+        // Set
+        Set<String> namesSet = new HashSet<>();
+        namesSet.add("Kati");
+        namesSet.add("Mati");
+        namesSet.add("Malle");
+        System.out.println(namesSet);
+
+        for(String name : namesSet) {
+            System.out.println(name);
+        }
+        namesSet.add("Malle");
+        System.out.println(namesSet);
+
+        // Map
+        Map<String, String> capitals = new HashMap<>();
+        capitals.put("Estonia", "Tallinn");
+        capitals.put("Finland", "Helsinki");
+        capitals.put("Sweden", "Stockholm");
+        System.out.println(capitals);
+        for(String countryName : capitals.keySet()) {
+            System.out.println(countryName + " - " + capitals.get(countryName));
+        }
+    }
+}
