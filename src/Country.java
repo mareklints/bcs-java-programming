249bce8e360c81cbd7caa59836e755f8feacc5a6
Marek Lints <marek.lints@gmail.com>
2020-05-31 13:00:21 +0300

    Klassid ja objektid

---
+++ src/Country.java
@@ -0,0 +1,13 @@
+public class Country {
+    public String name;
+    public double gdp;
+    public int population;
+
+    public double getGdpPerCapita() {
+        return this.gdp / this.population * 1_000_000;
+    }
+
+//    public static double getGdpPerCapita(Country country) {
+//        return country.gdp / country.population * 1_000_000;
+//    }
+}
---
+++ src/OopBasics.java
@@ -0,0 +1,31 @@
+public class OopBasics {
+    public static void main(String[] args) {
+        /*
+            NIMI		SKP (MUSD)	RAHVAARV
+            Austraalia	1376255		25725000
+            Eesti		31038		1328976
+            Saksamaa	3863344		83149300
+        */
+
+        Country australia = new Country();
+        australia.name = "Austraalia";
+        australia.gdp = 1376255;
+        australia.population = 25725000;
+
+        Country estonia = new Country();
+        estonia.name = "Eesti";
+        estonia.gdp = 31038;
+        estonia.population = 1328976;
+
+        System.out.println("Riik: " + australia.name);
+        System.out.println("SKP: " + australia.gdp);
+        System.out.println("SKP inimese kohta: " + australia.getGdpPerCapita());
+
+        System.out.println("---");
+
+        System.out.println("Riik: " + estonia.name);
+        System.out.println("SKP: " + estonia.gdp);
+        System.out.println("SKP inimese kohta: " + estonia.getGdpPerCapita());
+
+    }
+}
