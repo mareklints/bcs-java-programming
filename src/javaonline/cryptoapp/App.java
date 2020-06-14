eba0d844f3e0d1c2509f44ba44f8f28b03949a2e
Marek Lints <marek.lints@gmail.com>
2020-06-14 10:59:30 +0300

    Spiooniäpp (1)

---
+++ src/javaonline/cryptoapp/App.java
@@ -0,0 +1,37 @@
+package javaonline.cryptoapp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("resources/alfabeet.txt"));
+        // "A, Ü", "B, Ö"
+
+        // VÕTI ON MATI ALL
+        // V --> F
+        // Õ --> D
+        Map<String, String> converterMap = new HashMap<>();
+        for (String line : lines) {
+            String[] lineParts = line.split(", ");
+            converterMap.put(lineParts[0], lineParts[1]);
+        }
+
+        // VÕTI ON MATI ALL
+        String message = args[0];
+        String[] messageChars = message.split("");
+        String encryptedMessage = "";
+
+        for (String messageChar : messageChars) {
+            encryptedMessage = encryptedMessage + converterMap.get(messageChar.toUpperCase()); // "v" --> "V"
+        }
+
+        System.out.println("Krüpteerimist vajav sõnum: " + message);
+        System.out.println("Krüpteeritud sõnum: " + encryptedMessage);
+    }
+}
